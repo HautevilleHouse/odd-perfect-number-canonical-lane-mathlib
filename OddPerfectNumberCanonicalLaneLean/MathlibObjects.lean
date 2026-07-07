f81305fcfa5e83395dafb95d8602daabfa78cb95
HautevilleHouse <239203806+HautevilleHouse@users.noreply.github.com>
2026-07-07 06:51:38 -0400

    Canon

--- OddPerfectNumberCanonicalLaneLean/MathlibObjects.lean
+++ OddPerfectNumberCanonicalLaneLean/MathlibObjects.lean
@@ -1,11 +1,17 @@
 import OddPerfectNumberCanonicalLaneLean.TheoremStatement
 import CanonicalLaneMathlibCore
 import Mathlib.Data.Set.Basic
+import Mathlib.Data.Nat.Prime.Basic
 
 namespace HautevilleHouse
 namespace OddPerfectNumberCanonicalLaneLean
 
 open HautevilleHouse.CanonicalLaneMathlibCore
+
+universe u v w
+
+def MathlibPrimePredicate (n : Nat) : Prop :=
+  Nat.Prime n
 
 structure TheoremSpecificObject where
   sourceKey : String
@@ -13,15 +19,32 @@ structure TheoremSpecificObject where
   claimBoundary : String
 deriving Repr, DecidableEq
 
+structure UpstreamMathlibSubstrate where
+  primeSupport : Set Nat
+  polynomialObstruction : Nat -> Prop
+  diophantineGate : Prop
+  localGlobalArithmeticBridge : Prop
+  sourceBoundaryLedger : Set String
+
 structure AdmittedTheoremObject where
   object : TheoremSpecificObject
+  substrate : UpstreamMathlibSubstrate
   localWitness : String
   bridgeEvidence : String
+  primeOrDiophantineGateChecked : Prop
+  primeOrDiophantineGateWitness : primeOrDiophantineGateChecked
+  localGlobalArithmeticBridgeChecked : Prop
+  localGlobalArithmeticBridgeWitness : localGlobalArithmeticBridgeChecked
+  sourceBoundaryLedgerChecked : Prop
+  sourceBoundaryLedgerWitness : sourceBoundaryLedgerChecked
+  classicalRemainderCarried : formalizationCertificate.sourceConjectureClosureClaimed = false
   sourceKeyChecked : object.sourceKey = sourceRepository
   theoremObjectChecked : object.theoremObject = sourceDescription
 
-structure EndgameState where
+structure ClosureState where
   object : AdmittedTheoremObject
+  endpointSatisfied : Prop
+  remainderRecorded : Prop
 
 def theoremSpecificObject : TheoremSpecificObject := {
   sourceKey := sourceRepository,
@@ -30,7 +53,15 @@ def theoremSpecificObject : TheoremSpecificObject := {
 }
 
 def NativeBridgeClosed (O : AdmittedTheoremObject) : Prop :=
-  O.object.sourceKey = sourceRepository ∧ O.object.theoremObject = sourceDescription
+  O.object.sourceKey = sourceRepository ∧
+  O.object.theoremObject = sourceDescription ∧
+  O.primeOrDiophantineGateChecked ∧
+  O.localGlobalArithmeticBridgeChecked ∧
+  O.sourceBoundaryLedgerChecked ∧
+  (formalizationCertificate.sourceConjectureClosureClaimed = false)
+
+def ScopedClosure (O : AdmittedTheoremObject) : Prop :=
+  NativeBridgeClosed O
 
 end OddPerfectNumberCanonicalLaneLean
 end HautevilleHouse
